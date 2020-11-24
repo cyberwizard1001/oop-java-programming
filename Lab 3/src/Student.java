aa9adefd70f1c0bbb66aabea6cd46de688e84eeb
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2020-11-24 09:02:13 +0530

    Lab - November 24 - updated

---
+++ Lab 3/src/Student.java
@@ -0,0 +1,20 @@
+public class Student extends Person{
+
+    private String ID;
+
+    private static int IDVar = 0000;
+
+    public Student(String name, int age, String gender)
+    {
+        super(name,age,gender);
+        IDVar++;
+        String IDno = Integer.toString(IDVar);
+        ID = "S" + IDno;
+    }
+
+    @Override
+    public void count() {
+        super.count();
+        System.out.println("Number of students: " + IDVar);
+    }
+}
---
+++ Lab 3/src/t_Main.java
@@ -0,0 +1,7 @@
+public class t_Main {
+
+    static public void main(String[] args)
+    {
+
+    }
+}
